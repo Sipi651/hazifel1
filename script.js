780981aa3b8b6463550fbf1091f281b443a1583b
Sipi651 <siposarpad969@gmail.com>
2024-09-17 17:07:21 +0200

    Állapot/változás rövid leírása

---
+++ script.js
@@ -0,0 +1,12 @@
+let szovgesdoboz=document.getElementById("szoveg");
+let megszamolva=document.getElementById("szam");
+
+szovgesdoboz.addEventListener("input", function(){
+ let karakterek=this.value;
+ let karakterekszama=text.length;
+document.getElementById("szam").textContent='karszama'+karakterekszama
+
+});
+
+
+
